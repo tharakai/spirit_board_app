cb5c8279e0ee0d45bc750dd71d808197da5f5196
Tharakai <tharakai@freshdesk.com>
2015-08-07 16:12:15 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpiritBoardApp::Application.config.secret_token = 'c70f7598f2841ab6d96c37b4cae3e5ff6d615144d04e9f82c58ed7beb0f76a7fa344df58b4e705755a12d2f77a5a386d32eded44c23725812ca586a6313e4fc4'
