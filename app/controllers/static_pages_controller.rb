eb261f1cd589f423e47f5975fdb081bf68394aba
Tharakai <tharakai@freshdesk.com>
2015-08-10 13:28:58 +0530

    Initial layout

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,10 +2,15 @@ class StaticPagesController < ApplicationController
   def home
   end
 
-  def help
-  end
-
   def about
   end
 
+  def teams
+  end
+
+  def scores
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 SpiritBoardApp::Application.routes.draw do
-  get "static_pages/home"
+  root to: 'static_pages#home'
 
-  get "static_pages/help"
-
-  get "static_pages/about"
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
